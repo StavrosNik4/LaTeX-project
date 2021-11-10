d21d9ce5cce29e815c31a7b4fa7da0e98866dd24
Stavros Nikolaidis <stavniko@csd.auth.gr>
2021-11-10 11:12:52 +0200

    final

--- python code for the graphs/cos graph.py
+++ python code for the graphs/cos graph.py
@@ -9,26 +9,23 @@ for i in range(len(in_array_cos)):
     out_array_cos.append(math.cos(in_array_cos[i]))
     i += 1
    
+#printing the values for θ and cos(θ)
 print("in_array : ", in_array_cos)
 print("\nout_array : ", out_array_cos)
   
 # creating the chart
 plt.plot(in_array_cos, out_array_cos, color = 'red', marker = "o", label = "cos(θ)") 
-
 plt.title("Plot of cos(θ)") 
 plt.xlabel("θ") 
 plt.ylabel("cos(θ)") 
-
-plt.annotate('cos(pi/4)', xy =(-0.85, 0.681),
+# annotation for cos(-pi/4)
+plt.annotate('cos(-pi/4)', xy =(-0.85, 0.681),
                 xytext =(-0.30, 0.651), 
-                arrowprops = dict(facecolor ='red',
+                arrowprops = dict(facecolor ='black',
                                   shrink = 1),)
 
-plt.legend()
-
-
-
 plt.show()
+
 
 
 
