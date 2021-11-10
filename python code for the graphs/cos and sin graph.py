7cf6f962888e2d1697250f55ae7f642053c98f54
Stavros Nikolaidis <stavniko@csd.auth.gr>
2021-11-10 11:13:12 +0200

    final

--- python code for the graphs/cos and sin graph.py
+++ python code for the graphs/cos and sin graph.py
@@ -17,16 +17,32 @@ for i in range(len(in_array_cos)):
     out_array_sin.append(math.sin(in_array_sin[i]))
     i += 1
   
-   
-print("in_array : ", in_array_cos)
-print("\nout_array : ", out_array_cos)
+#printing the values for θ and cos(θ)   
+print("in_array_cos : ", in_array_cos)
+print("\nout_array_cos : ", out_array_cos)
+
+
+#printing the values for θ and sin(θ) 
+print("in_array_sin : ", in_array_sin)
+print("\nout_array_sin : ", out_array_sin)
   
 # creating the graph
-plt.plot(in_array_cos, out_array_cos, color = 'blue', marker = "o") 
-plt.plot(in_array_sin, out_array_sin, color = 'green', marker = "o")
-
+plt.plot(in_array_cos, out_array_cos, color = 'blue', marker = "o", label='cos(θ)') 
+plt.plot(in_array_sin, out_array_sin, color = 'green', marker = "o", label='sin(θ)')
 plt.title("Plot of cos(θ) and sin(θ)") 
 plt.xlabel("θ") 
 plt.ylabel("cos(θ) and sin(θ)") 
+
+# annotations for cos(-pi/4) and sin(-pi/4)
+plt.annotate('cos(-pi/4)', xy =(-0.85, 0.681),
+                xytext =(-0.30, 0.651), 
+                arrowprops = dict(facecolor ='black',
+                                  shrink = 1),)
+plt.annotate('sin(-pi/4)', xy =(-0.85, -0.710),
+                xytext =(-0.2, -0.730), 
+                arrowprops = dict(facecolor ='black',
+                                  shrink = 1),)
+
+plt.legend()
 
 plt.show()
